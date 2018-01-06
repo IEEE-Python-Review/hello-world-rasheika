44d7eedf0a9dbf3954f8e6cf6667b7ca7e56460e
Lomar Lilly <lithiumlilly3@gmail.com>
2018-01-06 00:15:40 -0500

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,12 +1,5 @@
 '''
-Write a program that will display the following
-Hello World!
-Hello Again
-I like typing this.
-This is fun.
-Yay! Printing.
-I'd much rather you 'not'.
-I "said" do not touch this.
+Write a program that will display Hello World!!!
 '''
 
 # Code goes here!
