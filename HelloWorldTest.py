e25c2749375242898836c06cb1e997eb6f94bd60
Lomar Lilly <lithiumlilly3@gmail.com>
2018-01-06 00:21:16 -0500

    Update HelloWorldTest.py

--- HelloWorldTest.py
+++ HelloWorldTest.py
@@ -4,14 +4,14 @@ from unittest.mock import patch
 
 class TestHelloWorld(unittest.TestCase):
 
-	@patch('builtins.input', return_value="Hello World")
+	@patch('builtins.input', return_value="Hello World!!!")
 	def test_HelloWorld(self, sth):
 		try:
 			out = io.StringIO()
 			sys.stdout = out
 			import HelloWorld
 			output = out.getvalue().strip()
-			assert output == 'Hello World'
+			assert output == 'Hello World!!!'
 		finally:
 			pass
 
